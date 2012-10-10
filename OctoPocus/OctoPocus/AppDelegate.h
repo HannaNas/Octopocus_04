c5e3605dea03747ffa81c565e6e924e5935e75ef
victor <victorvj26@gmail.com>
2012-10-10 16:39:20 +0200

    Base project
    Base

---
+++ OctoPocus/OctoPocus/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OctoPocus
+//
+//  Created by Victor Valle Juarranz on 10/10/12.
+//  Copyright (c) 2012 Hanna&Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
